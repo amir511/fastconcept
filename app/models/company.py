709e69fd8f8eb975b7bd3c6f56c7e13e9c18944b
Amir <amir.anwar.said@gmail.com>
2021-04-27 17:37:35 +0200

    Add company model

---
+++ app/models/company.py
@@ -0,0 +1,14 @@
+from app.models.db_setup.session import BaseModel
+import sqlalchemy as sql
+
+
+class Company(BaseModel):
+    __tablename__ = 'companies'
+
+    id = sql.Column(sql.Integer, primary_key=True, index=True)
+    name = sql.Column(sql.String, unique=True)
+
+    def __repr__(self):
+        return f'Company: {self.name}'
+
+
