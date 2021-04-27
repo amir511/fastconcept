5e91880e860f2b88f58f538d2f05a6e457f2d46f
Amir <amir.anwar.said@gmail.com>
2021-04-27 14:51:28 +0200

    Add database session and connections

---
+++ app/models/db/session.py
@@ -0,0 +1,11 @@
+from app.settings import settings
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = f"postgresql://{settings.DATABASE.get('username')}:{settings.DATABASE.get('password')}@{settings.DATABASE.get('database_server')}:{settings.DATABASE.get('port')}/{settings.DATABASE.get('database_name')}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+BaseModel = declarative_base()
