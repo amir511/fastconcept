1429660658abb67492ab106cdb813f066814744b
Amir <amir.anwar.said@gmail.com>
2021-04-27 22:07:18 +0200

    Add description field to company table

--- app/models/company.py
+++ app/models/company.py
@@ -7,6 +7,7 @@ class Company(BaseModel):
 
     id = sql.Column(sql.Integer, primary_key=True, index=True)
     name = sql.Column(sql.String, unique=True)
+    description = sql.Column(sql.String, nullable=True)
 
     def __repr__(self):
         return f'Company: {self.name}'
