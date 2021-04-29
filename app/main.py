ba704bdba55cd40655ceacb16b14ca5fe42a9012
Amir <amir.anwar.said@gmail.com>
2021-04-29 17:11:09 +0200

    Add User views

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.views import company
+from app.views import company, user
 
 app = FastAPI()
 app.include_router(company.router)
+app.include_router(user.router)
 
 @app.get('/hello')
 async def hello_world():
---
+++ app/views/user.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, Depends, status, HTTPException, Response
+from app.models.db_setup.session import get_db
+from app.serializers.user import UserSerializerIn, UserSerializerOut
+from app.models.user import User
+from sqlalchemy.orm import Session
+
+router = APIRouter(prefix='/user', tags=['Users'])
+
+# CRUD
+@router.post('/create/', response_model=UserSerializerOut, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserSerializerIn, db:Session=Depends(get_db)):
+    user_obj = User(**user.dict())
+    user_obj.set_password(user_obj.password)
+    db.add(user_obj)
+    db.commit()
+    db.refresh(user_obj)
+    return user_obj
+
+@router.get('/get/{user_id}', response_model=UserSerializerOut)
+def get_user(user_id:int, db:Session=Depends(get_db)):
+    user_obj = db.query(User).get(user_id)
+    if not user_obj:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='User not found!')
+    return user_obj
+
+@router.get('/get/all/', response_model=list[UserSerializerOut])
+def get_all_users(db:Session=Depends(get_db)):
+    return db.query(User).all()
+
+@router.patch('/update/{user_id}', response_model=UserSerializerOut)
+def update_user(user_id:int, user:UserSerializerIn, db:Session=Depends(get_db)):
+    user_obj = db.query(User).get(user_id)
+    if not user_obj:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='User not found!')
+    for k, v in user.dict(exclude_unset=True).items():
+        if k == 'password':
+            user_obj.set_password(v)
+        else:
+            setattr(user_obj, k, v)
+    db.commit()
+    db.refresh(user_obj)
+    return user_obj
+
+@router.delete('/delete/{user_id}', status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+def delete_user(user_id:int, db:Session=Depends(get_db)):
+    user_obj = db.query(User).get(user_id)
+    if not user_obj:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='User not found!')
+    else:
+        db.delete(user_obj)
+        db.commit()
